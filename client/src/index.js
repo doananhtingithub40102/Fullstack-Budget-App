730804394ba8839411438be7ad11742d8e1eddfe
doananhtingithub40102 <doananhtin2001st@gmail.com>
2023-03-03 22:32:24 +0700

    Merging App.js file and index.js file into one

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,30 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App"
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Navbar from "./components/Navbar"
+import Dashboard from "./components/Dashboard"
+import { AccountProvider } from "./utils/AccountProvider"
+import { TransactionProvider } from "./utils/TransactionProvider"
+
+import "./App.css"
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/js/bootstrap.bundle"
+
+const App = () => {
+    return (
+        <div className="container">
+            <AccountProvider>
+                <Navbar />
+                <TransactionProvider>
+                    <Routes>
+                        <Route path="/" element={<Dashboard />} />
+                    </Routes>
+                </TransactionProvider>
+            </AccountProvider>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
